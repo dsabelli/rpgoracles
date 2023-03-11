037738fa6bbe33f373e935d2978d0bec52c54d89
dsabelli <dsabelli3@gmail.com>
2023-03-11 06:31:39 -0500

    change params

--- components/Card.vue
+++ components/Card.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="card w-64 bg-base-100 shadow-xl">
     <figure class="px-10 pt-10">
-      <img :src="props.dataTable.image" alt="Shoes" class="rounded-xl w-48" />
+      <img :src="dataTable.image" alt="Shoes" class="rounded-xl w-48" />
     </figure>
     <div class="card-body items-center text-center">
-      <h2 class="card-title">{{ props.dataTable.name }}</h2>
-      <p>{{ props.dataTable.description }}</p>
+      <NuxtLink :to="`${path}/${dataTable.document}-${dataTable.id}`">
+        <h2 class="card-title">{{ dataTable.name }}</h2></NuxtLink
+      >
+      <p>{{ dataTable.description }}</p>
       <div class="card-actions">
-        <button class="btn btn-primary">
-          <NuxtLink :to="`${path}/${props.dataTable.id}`">Read More</NuxtLink>
-        </button>
+        <NuxtLink :to="`${path}/${dataTable.document}-${dataTable.id}`"
+          ><button class="btn btn-primary">Read More</button></NuxtLink
+        >
       </div>
     </div>
   </div>
@@ -20,7 +22,7 @@ import { DataTable, OracleTable } from "~~/types";
 const route = useRoute();
 const path = route.fullPath;
 
-const props = defineProps({
+const { dataTable } = defineProps({
   dataTable: { type: Object as PropType<DataTable>, required: true },
 });
 </script>
