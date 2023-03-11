cac608ae80236af1c25ce9f9e3629ee09c55ca6d
dsabelli <dsabelli3@gmail.com>
2023-03-11 06:31:23 -0500

    change params

---
+++ pages/tables/fantasy/dark/[document]-[id].vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Table v-if="dataTable" :dataTable="dataTable[0]"></Table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { DataTable } from "~~/types";
+
+const { id } = useRoute().params;
+const { data: dataTable } = await useFetch<DataTable[]>(
+  `http://localhost:3000/tables/?id=${id}`
+);
+
+if (!dataTable.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Product not found",
+    fatal: true,
+  });
+}
+</script>
+
+<style scoped></style>
