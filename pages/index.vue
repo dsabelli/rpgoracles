f9ba5d02c1fbea9c6efb35883e261330268f3613
dsabelli <dsabelli3@gmail.com>
2023-03-21 21:08:27 -0400

    test server api

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="min-h-screen">
     <Hero />
-    <div class="h-1/4">Test</div>
+    <div class="h-1/4">{{ info }}</div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { data: api } = await useFetch("/api/hiMom");
+
+const info = ref(api);
+</script>
 
 <style scoped></style>
