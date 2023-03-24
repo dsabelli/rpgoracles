2c61acb4688791aa5bbd55285ca44fcf86d01d63
dsabelli <dsabelli3@gmail.com>
2023-03-24 10:26:06 -0400

    remove test data

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="min-h-screen">
     <Hero />
-    <div class="h-1/4">{{ info }}</div>
+    <div class="h-1/4">test</div>
   </div>
 </template>
 
-<script setup lang="ts">
-const { data: api } = await useFetch("/api/hiMom");
-const info = ref(api);
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
