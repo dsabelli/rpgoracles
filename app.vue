43c345639fd055044a757996f1f03d29fdc42352
dsabelli <dsabelli3@gmail.com>
2023-03-25 15:59:49 -0400

    call api to store on page load

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtLayout>
+    <div class="min-h-screen">
+      <NuxtPage /></div
+  ></NuxtLayout>
+</template>
+
+<script setup lang="ts">
+const tagStore = useTagStore();
+const metaTablesStore = useMetaTablesStore();
+const documentsStore = useDocumentStore();
+
+metaTablesStore.getMetaTables();
+tagStore.getMainTags();
+tagStore.getSubTags();
+documentsStore.getDocuments();
+</script>
+
+<style scoped></style>
