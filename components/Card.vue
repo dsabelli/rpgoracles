145181247ba42fe7e248f323592e09445d1cdc7b
dsabelli <dsabelli3@gmail.com>
2023-03-25 16:02:21 -0400

    take table,tags,doc as props, set path name w/prop

--- components/Card.vue
+++ components/Card.vue
@@ -1,21 +1,22 @@
 <template>
-  <div
-    v-if="subtags"
+  <!-- <div
+    v-if="subTags"
     class="card w-64 bg-base-100 shadow-xl"
     :class="
-      isParentPath(route, subtags)
+      isParentPath(route, subTags)
         ? ''
-        : isMatchingSubpath(route, dataTable)
+        : isMatchingSubpath(route, metaTable)
         ? ''
         : 'hidden'
     "
-  >
+  > -->
+  <div class="card w-72 bg-base-100 shadow-xl">
     <figure class="px-10 pt-10">
-      <img :src="dataTable.image" alt="Shoes" class="rounded-xl w-48" />
+      <img :src="metaTable.meta_image" alt="Shoes" class="rounded-xl w-48" />
     </figure>
     <div class="card-body items-center text-center">
-      <h2 class="card-title">{{ dataTable.name }}</h2>
-      <p>{{ dataTable.description }}</p>
+      <h2 class="card-title">{{ metaTable.meta_name }}</h2>
+      <p>{{ metaTable.meta_desc }}</p>
       <div class="card-actions">
         <NuxtLink :to="path"
           ><button class="btn btn-primary">Read More</button></NuxtLink
@@ -26,17 +27,19 @@
 </template>
 
 <script setup lang="ts">
-import { DataTable, Subtag } from "~~/types";
+import { doc_types, main_tags, meta_tables, sub_tags } from ".prisma/client";
+import { PropType } from "vue";
+
 const route = useRoute();
 
-const { dataTable } = defineProps({
-  dataTable: { type: Object as PropType<DataTable>, required: true },
+const { metaTable, subTag, mainTag, document } = defineProps({
+  metaTable: { type: Object as PropType<meta_tables>, required: true },
+  subTag: { type: Object as PropType<sub_tags>, required: false },
+  mainTag: { type: Object as PropType<main_tags>, required: false },
+  document: { type: Object as PropType<doc_types>, required: false },
 });
 
-const { data: subtags } = await useFetch<Subtag[]>(
-  `http://localhost:3001/subtags`
-);
-if (!subtags.value) {
+if (!metaTable) {
   throw createError({
     statusCode: 404,
     statusMessage: "Page not found",
@@ -48,15 +51,16 @@ if (!subtags.value) {
 //else go to the specific documents path
 let path = ref("");
 
-const getPath = (): string =>
-  route.params.document
-    ? `${route.fullPath}-${dataTable.id}`
-    : subtags.value &&
-      isParentPath(route, subtags.value) &&
-      dataTable.subtag.length
-    ? `${route.fullPath}/${dataTable.subtag}/${dataTable.document}-${dataTable.id}`
-    : `${route.fullPath}/${dataTable.document}-${dataTable.id}`;
+path.value = `/tables/${mainTag?.tag_path}/${subTag?.tag_path}/${document?.doc_path}-${metaTable.id}`;
+// const getPath = (): string =>
+//   route.params.document
+//     ? `${route.fullPath}-${metaTable.id}`
+//     :
+//       isParentPath(route, subtags.value) &&
+//       metaTable.subtag.length
+//     ? `${route.fullPath}/${metaTable.subtag}/${metaTable.document}-${metaTable.id}`
+//     : `${route.fullPath}/${metaTable.document}-${metaTable.id}`;
 
-path.value = getPath();
+// path.value = getPath();
 </script>
 <style scoped></style>
