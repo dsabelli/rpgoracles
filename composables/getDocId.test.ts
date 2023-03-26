1adc9fc2a9ccfe46a39cc6fed872cfc4ccfaf811
dsabelli <dsabelli3@gmail.com>
2023-03-26 15:49:13 -0400

    tests for composables

--- composables/getDocId.test.ts
+++ composables/getDocId.test.ts
@@ -1,13 +1,16 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeAll } from "vitest";
 import prisma from "../prisma/client";
 import { getDocId } from "./getDocId";
-
-const documents = await prisma.doc_types.findMany({});
+import { doc_types } from "@prisma/client";
 
 const param1 = "names";
 const param2 = "notADocument";
 
 describe("check to see if the document ID is found from the route param string", async () => {
+  let documents: doc_types[];
+  beforeAll(async () => {
+    documents = await prisma.doc_types.findMany({});
+  });
   it("returns the ID when a proper match is found", () => {
     expect(getDocId(documents, param1)).toBe(1);
   });
