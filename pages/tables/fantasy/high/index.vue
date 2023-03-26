674358a75c03073f76b4fae96a0bcd23116c38a4
dsabelli <dsabelli3@gmail.com>
2023-03-26 15:33:27 -0400

    move getters to composables

--- pages/tables/fantasy/high/index.vue
+++ pages/tables/fantasy/high/index.vue
@@ -2,25 +2,24 @@
   <h2>High Fantasy</h2>
   <div v-if="metaTables && subTags" v-for="t in metaTables">
     <Card
-      v-if="t.subtag_id === getSubTagId"
+      v-if="t.subtag_id === getSubTagId(subTags, useRoute().path)"
       :metaTable="t"
       :subTag="subTags.find((s) => s.id === t.subtag_id)"
       :mainTag="mainTags.find((m) => m.id === t.tag_id)"
       :document="documents.find((d) => d.id === t.doc_id)"
     />
-    <p>{{ getSubTagId }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
-
+import { getSubTagId } from "~~/composables/getSubTagId";
 const metaTablesStore = useMetaTablesStore();
 const tagStore = useTagStore();
 const documentStore = useDocumentStore();
 
 const { metaTables } = storeToRefs(metaTablesStore);
-const { mainTags, subTags, getSubTagId, getMainTagId } = storeToRefs(tagStore);
+const { mainTags, subTags } = storeToRefs(tagStore);
 const { documents } = storeToRefs(documentStore);
 
 if (!metaTables || !subTags || !mainTags || !documents) {
