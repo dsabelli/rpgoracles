54c764b68664efb1c5dfb5b15d8bcad9e9647858
dsabelli <dsabelli3@gmail.com>
2023-03-28 18:19:38 -0400

    set up subtag dynamic

---
+++ server/api/sub-tags/[tag].ts
@@ -0,0 +1,7 @@
+import prisma from "~~/prisma/client";
+
+export default defineEventHandler(async (e) => {
+  return await prisma.sub_tags.findFirst({
+    where: { tag_path: e.context.params?.tag },
+  });
+});
