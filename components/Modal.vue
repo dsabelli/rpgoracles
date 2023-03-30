8aaf82612aa29c03ffa5133b59a80d9672c3d0ca
dsabelli <dsabelli3@gmail.com>
2023-03-30 15:19:18 -0400

    add modal and table roll function

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <!-- The button to open modal -->
     <label for="roll-modal" class="btn" @click="rollTable">Roll Table</label>
 
-    <!-- Put this part before </body> tag -->
     <input type="checkbox" id="roll-modal" class="modal-toggle" />
     <label for="roll-modal" class="modal cursor-pointer">
       <label class="modal-box relative" for="s">
@@ -22,6 +20,7 @@ const { tableEntries } = defineProps({
   tableEntries: { type: Object as PropType<table_entries[]>, required: true },
 });
 const result: Ref<table_entries> | Ref<undefined> = ref();
+
 const rollTable = () => {
   const rand = Math.floor(Math.random() * tableEntries.length);
   result.value = tableEntries[rand];
