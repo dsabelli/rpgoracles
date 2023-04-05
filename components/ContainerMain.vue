926adf6bc48142fae340e3ac9ec130bf144e93f2
dsabelli <dsabelli3@gmail.com>
2023-04-05 13:29:31 -0400

    move svgs into Vue components

--- components/ContainerMain.vue
+++ components/ContainerMain.vue
@@ -6,27 +6,13 @@
       <div class="w-full navbar bg-base-100 gap-4 pl-0 justify-between">
         <div class="lg:hidden">
           <label for="my-drawer-3" class="hover:cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              class="inline-block w-8 h-8 stroke-current"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
+            <MenuIcon />
           </label>
         </div>
         <div class="">
-          <NuxtLink class="flex items-center lg:gap-4" to="/"
-            ><img
-              src="~/assets/static/logo.svg"
-              class="w-16 mt-2 sm:w-24 lg:-ml-2"
-            />
+          <NuxtLink class="flex items-center lg:gap-4" to="/">
+            <Logo styles="w-16 h-16 mt-2 sm:w-24 sm:h-24 lg:-ml-2" />
+
             <h2 class="text-xl sm:text-3xl">RPG&nbsp;Oracles</h2></NuxtLink
           >
         </div>
