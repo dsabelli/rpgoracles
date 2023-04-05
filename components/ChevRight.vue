085eff87efe597f895ec19160e9deab993db266c
dsabelli <dsabelli3@gmail.com>
2023-04-05 13:29:05 -0400

    move svgs into Vue components

---
+++ components/ChevRight.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-chevron-right"
+    :class="styles"
+  >
+    <polyline points="9 18 15 12 9 6"></polyline>
+  </svg>
+</template>
+
+<script setup lang="ts">
+const { styles } = defineProps({
+  styles: { type: String, required: false },
+});
+</script>
+
+<style scoped></style>
---
+++ components/MenuIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    class="inline-block w-8 h-8 stroke-current"
+    :class="styles"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M4 6h16M4 12h16M4 18h16"
+    ></path>
+  </svg>
+</template>
+
+<script setup lang="ts">
+const { styles } = defineProps({
+  styles: { type: String, required: false },
+});
+</script>
+
+<style scoped></style>
