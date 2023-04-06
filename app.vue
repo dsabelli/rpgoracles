c993147122c6c00f657190aef566526424f58059
dsabelli <dsabelli3@gmail.com>
2023-04-06 14:38:16 -0400

    remove footer, add loading indicator

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
   <NuxtLayout>
+    <NuxtLoadingIndicator
+      color="repeating-linear-gradient(to right,#00dc82 0%,#34cdfe 50%,#0047e1 100%)"
+    />
     <NuxtPage />
-    <Footer />
   </NuxtLayout>
 </template>
 
