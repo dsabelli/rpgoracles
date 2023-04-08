38f3016ccb9c19b298db479c3470ee6b7480f41d
dsabelli <dsabelli3@gmail.com>
2023-04-08 13:45:11 -0400

    change navbar to grid

--- components/ContainerMain.vue
+++ components/ContainerMain.vue
@@ -3,20 +3,25 @@
     <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content flex flex-col w-full">
       <!-- Navbar -->
-      <div class="w-full navbar bg-base-100 gap-4 pl-0 justify-between">
-        <div class="lg:hidden">
+      <div
+        class="w-full grid grid-cols-8 justify-items-center items-center bg-base-100 gap-4 pl-0"
+      >
+        <div class="lg:hidden col-span-1">
           <label for="my-drawer-3" class="hover:cursor-pointer">
             <MenuIcon />
           </label>
         </div>
-        <div class="">
-          <NuxtLink class="flex items-center lg:gap-4" to="/">
-            <Logo styles="w-16 h-16 mt-2 sm:w-24 sm:h-24 lg:-ml-2" />
 
-            <h2 class="text-xl sm:text-3xl">RPG&nbsp;Oracles</h2></NuxtLink
-          >
-        </div>
-        <div class="hidden lg:block">
+        <NuxtLink
+          class="flex items-center lg:gap-4 col-span-6 lg:col-span-2"
+          to="/"
+        >
+          <Logo styles="w-16 h-16 mt-2 sm:w-24 sm:h-24 lg:-ml-2" />
+
+          <h2 class="text-xl sm:text-2xl">RPG&nbsp;Oracles</h2></NuxtLink
+        >
+
+        <div class="hidden lg:block w-full lg:col-span-6">
           <NavDesktop />
         </div>
       </div>
