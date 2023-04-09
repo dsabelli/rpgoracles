1918e83b834d27a40240ed6f141fc21cc28b6cd7
dsabelli <dsabelli3@gmail.com>
2023-04-09 11:05:05 -0400

    add copy, change width

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="hero h-screen max-w-screen-lg bg-base-100">
-    <div class="flex hero-content flex-col lg:flex-row gap-8">
-      <div>
+    <div
+      class="flex hero-content flex-col lg:flex-row w-full justify-around pr-8"
+    >
+      <div class="lg:w-96">
         <H1 class="font-bold">RPG Oracles</H1>
-        <p class="py-6">
-          Some random text that isn't lorem so chrome based browsers will stop
-          asking if I want to translate the page from Latin to English!
+        <p class="py-6 text-xl">
+          A tool for Solo RPGers and Dungeon Master's alike. RPG Oracles offers
+          an extensive range of RPG oracles and tables across all genres.
         </p>
       </div>
       <Icosahedron />
